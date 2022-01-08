74d03ab5ebc2bbc8c16b1b2873f7b7668970319e
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-01-08 16:49:01 +0000

    updating files for strings and lists

--- 1-python-crash-course/week-4-lists/strings-and-lists.py
+++ 1-python-crash-course/week-4-lists/strings-and-lists.py
@@ -1,6 +1,7 @@
 filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
 # Generate newfilenames as a list containing the new filenames
 # using as many lines of code as your chosen method requires.
+# any file that ends with .hpp extension needs to be updated to .h instead
 newfilenames = []
 for file in filenames:
     if "hpp" in file:
